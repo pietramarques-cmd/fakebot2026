62802046cfc9184936ba8f43cfb5f02f0456e467
pietramarques-cmd <pietramarquesdonascimento6@gmail.com>
2026-07-18 10:56:36 -0300

    Arquivo

---
+++ script.js
@@ -0,0 +1,16 @@
+const lista = [
+    "Olá, bom dia!",
+    "Tudo bem?",
+    "Estamos no ano de copa.",
+    "Vamos torcer juntos!",
+    "O hexa vem! Vai Brasil!",
+    "Até logo."
+]
+
+
+var n = 0;
+function funcao() {
+    resposta.innerHTML = lista [n];
+    n++;
+    if(n>5) n=0;
+}
